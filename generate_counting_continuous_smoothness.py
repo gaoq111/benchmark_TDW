6570df31ca5a9673bb926c69d0ce75dedeb60db2
Xinyu Fang <CharlesFang207@outlook.com>
2024-09-06 01:39:35 -0400

    Add continuous counting script for smoothness

--- generate_counting_continuous_smoothness.py
+++ generate_counting_continuous_smoothness.py
@@ -23,12 +23,11 @@ from tdw.add_ons.interior_scene_lighting import InteriorSceneLighting
 # DISPLAY=:4 ./TDW.x86_64 -port 1071
 
 # Run this script
-# python3 generate_counting_discrete_backup.py --output_path ./output
+# python3 generate_counting_continuous_smoothness.py --output_path ./output
 
 # TODO:
 # 1. reconstrcut the code.
-# 2. remove defalut light in the scenes.
-# 3. determine how to describe the color (yellow or brown)
+# 2. remove the default light in the scenes.
 
 
 def get_floor_height(controller: Controller) -> float:
@@ -49,7 +48,7 @@ def get_floor_height(controller: Controller) -> float:
             return hit_position[1]  
     
     # If no hit is detected, return a default floor height
-    return 0
+    return 0  
 
 def main(args):
     output_path = args.output_path
@@ -59,11 +58,11 @@ def main(args):
 
     # Add interior lighting
     interior_lighting = InteriorSceneLighting()
-    c.add_ons.append(interior_lighting) 
+    c.add_ons.append(interior_lighting)  
 
     # Define camera views
     camera_positions = {
-        "top": {"x": 0, "z": -0.2, "y": 3.0},
+        "top": {"x": 0, "z": 0, "y": 3.0},
         "left": {"x": -3.0, "z": 0, "y": 0.5},
         "right": {"x": 3.0, "z": 0, "y": 0.5},
         "front": {"x": 0, "z": -3.0, "y": 0.5},
@@ -77,7 +76,7 @@ def main(args):
     object_colors = {
         "red": {"r": 1.0, "g": 0.2, "b": 0.2},  # Red
         "green": {"r": 0.2, "g": 1.0, "b": 0.2},  # Green
-        "brown": {"r": 1.0, "g": 1.0, "b": 0.0}   # Brown
+        "yellow": {"r": 1.0, "g": 1.0, "b": 0.0}   # Yellow
     }
 
     
@@ -85,14 +84,13 @@ def main(args):
 
     special_lib = ['prim_cone', 'prim_cube', 'prim_cyl', 'prim_sphere']
 
-    object_materials = ["limestone_white", "metal_brushed_copper", "wood_american_cherry"]
+    object_materials = ["metal_brushed_copper", "limestone_white", "sand_covered_stone_ground"]
     material_tuples = list(itertools.combinations(object_materials, 2))
 
     images_info = {}
 
     images_info["shape_section"] = []    
     images_info["color_section"] = []
-    images_info["material_section"] = []
 
     image_id = 0
 
@@ -114,9 +112,9 @@ def main(args):
 
                         # Initialize scene
                         commands.append(c.get_add_scene(scene))
-
                         c.communicate(commands)
 
+                        # Get floor height
                         floor_height = get_floor_height(c)
 
                         # Setup camera
@@ -128,6 +126,7 @@ def main(args):
                                     "front": {"x": 0, "z": -2.6, "y": 0.5},
                                     "back": {"x": 0, "z": 2.6, "y": 0.5},
                                 }
+
                         camera = ThirdPersonCamera(position=camera_positions[camera_position], avatar_id=camera_position, look_at={"x": 0, "y": 0, "z": 0})
                         c.add_ons.append(camera)
 
@@ -138,61 +137,71 @@ def main(args):
                         os.makedirs(image_folder, exist_ok=True)
                         c.add_ons.append(ImageCapture(path=image_folder, avatar_ids=[camera.avatar_id], png=True))
 
-
-                        obj_num_1 = random.randint(1, 4)
-                        obj_num_2 = obj_num_1
-                        while obj_num_2 == obj_num_1:
-                            obj_num_2 = random.randint(1, 4)
-
                         obj_type = 0
 
                         image_info = {}
                         objects_info = []
 
+                        positions = []
+
                         for object_name in tqdm(selected_obj_names, desc="Processing objects", leave=False):
                             lib = "models_special.json"
                             model_record = ModelLibrarian(lib).get_record(object_name)
 
-                            obj_num = obj_num_1 if obj_type == 0 else obj_num_2
+                            # obj_num = obj_num_1 if obj_type == 0 else obj_num_2
                             material = material_tuple[0] if obj_type == 0 else material_tuple[1]
                             color = color_1 if obj_type == 0 else color_2
                             color_name = color_name_1 if obj_type == 0 else color_name_2
 
-                            for _ in range(obj_num):
-                                object_id = c.get_unique_id()
+                            # for _ in range(obj_num):
+                            object_id = c.get_unique_id()
 
-                                position = {
-                                    "x": random.uniform(-1.0, 1.0),
-                                    "y": floor_height + 0.02,
-                                    "z": random.uniform(-1.0, 1.0)
-                                }
+                            position = {
+                                "x": random.uniform(-0.8, 0.8),
+                                "y": floor_height +0.02,
+                                "z": random.uniform(-0.8, 0.8)
+                            }
 
-                                # Place object with physics and check for collisions
-                                commands.extend(c.get_add_physics_object(model_name=object_name,
-                                                                    library=lib,
-                                                                    position=position,
-                                                                    default_physics_values=False,
-                                                                    scale_factor={"x": 0.2, "y": 0.2, "z": 0.2},
-                                                                    object_id=object_id))
-                                
-                                # Set the object's material
-                                commands.extend(TDWUtils.set_visual_material(c=c, substructure=model_record.substructure, material=material, object_id=object_id))
-                                commands.append({
-                                    "$type": "set_color",
-                                    "id": object_id,
-                                    "color": color  
-                                })
+                            if scene == "tdw_room":
+                                position["y"] += 0.01
+
+                            if positions == []:
+                                positions.append(position)
+                            else:
+                                if camera_position == "left" or camera_position == "right" or camera_position == "top":
+                                    while abs(position["z"] - positions[0]["z"]) < 0.3:
+                                        position["z"] = random.uniform(-0.8, 0.8)
+                                elif camera_position == "front" or camera_position == "back":
+                                    while abs(position["x"] - positions[0]["x"]) < 0.3:
+                                        position["x"] = random.uniform(-0.8, 0.8)
+
+                                positions.append(position)
+
+                            # Place object with physics and check for collisions
+                            commands.extend(c.get_add_physics_object(model_name=object_name,
+                                                                library=lib,
+                                                                position=position,
+                                                                default_physics_values=False,
+                                                                scale_factor={"x": 0.4, "y": 0.4, "z": 0.4},
+                                                                object_id=object_id))
+                            
+                            # Set the object's material
+                            commands.extend(TDWUtils.set_visual_material(c=c, substructure=model_record.substructure, material=material, object_id=object_id))
+                            commands.append({
+                                "$type": "set_color",
+                                "id": object_id,
+                                "color": color  
+                            })
 
                             # Record object info
                             objects_info.append({
                                 "name": object_name,
-                                "obj_num": obj_num,
+                                # "obj_num": obj_num,
                                 "material": material,
                                 "color": color_name
                             })
 
                             obj_type += 1
-
 
                         # Render the image
                         c.communicate(commands)
@@ -204,22 +213,22 @@ def main(args):
 
                         images_info["shape_section"].append(copy.deepcopy(image_info))
                         images_info["color_section"].append(copy.deepcopy(image_info))
-                        images_info["material_section"].append(copy.deepcopy(image_info))
+                        # images_info["material_section"].append(copy.deepcopy(image_info))
 
                         object_shape_1 = objects_info[0]["name"].split("_")[1]
                         object_shape_2 = objects_info[1]["name"].split("_")[1]
-                        images_info["shape_section"][-1]["question"] = f"Which are more numerous in the image, {object_shape_1}s or {object_shape_2}s? Answer with the letter of your choice: A. {object_shape_1}s B. {object_shape_2}s"
-                        images_info["shape_section"][-1]["gt_answer"] = "A" if objects_info[0]["obj_num"] > objects_info[1]["obj_num"] else "B"
+                        images_info["shape_section"][-1]["question"] = f"Which object has a smoother surface in the image, the {object_shape_1} or the {object_shape_2}? Answer with the letter of your choice: A. {object_shape_1} B. {object_shape_2}"
+                        images_info["shape_section"][-1]["gt_answer"] = "A" if object_materials.index(objects_info[0]["material"]) < object_materials.index(objects_info[1]["material"]) else "B"
 
                         object_color_1 = objects_info[0]["color"]
                         object_color_2 = objects_info[1]["color"]                    
-                        images_info["color_section"][-1]["question"] = f"Which color has more objects in the image, {object_color_1} or {object_color_2}? Answer with the letter of your choice: A. {object_color_1} B. {object_color_2}"
-                        images_info["color_section"][-1]["gt_answer"] = "A" if objects_info[0]["obj_num"] > objects_info[1]["obj_num"] else "B"         
+                        images_info["color_section"][-1]["question"] = f"Which object has a smoother surface in the image, the {object_color_1} one or the {object_color_2} one? Answer with the letter of your choice: A. {object_color_1} B. {object_color_2}"
+                        images_info["color_section"][-1]["gt_answer"] = "A" if object_materials.index(objects_info[0]["material"]) < object_materials.index(objects_info[1]["material"]) else "B"        
 
-                        object_material_1 = objects_info[0]["material"].split("_")[0]
-                        object_material_2 = objects_info[1]["material"].split("_")[0]
-                        images_info["material_section"][-1]["question"] = f"Which material has more objects in the image, {object_material_1} or {object_material_2}? Answer with the letter of your choice: A. {object_material_1} B. {object_material_2}"
-                        images_info["material_section"][-1]["gt_answer"] = "A" if objects_info[0]["obj_num"] > objects_info[1]["obj_num"] else "B"           
+                        # object_material_1 = objects_info[0]["material"].split("_")[0]
+                        # object_material_2 = objects_info[1]["material"].split("_")[0]
+                        # images_info["material_section"][-1]["question"] = f"Which material has more objects in the image, {object_material_1} or {object_material_2}? Answer with the letter of your choice: A. {object_material_1} B. {object_material_2}"
+                        # images_info["material_section"][-1]["gt_answer"] = "A" if object_materials.index(objects_info[0]["material"]) < object_materials.index(objects_info[1]["material"]) else "B"          
 
                         # Copy image
                         source_path = f"{image_folder}/{camera_position}/img_0000.png"
@@ -234,7 +243,7 @@ def main(args):
                         image_id += 1
 
     # Save object info to JSON
-    with open(os.path.join(output_path, "discrete_counting_info.json"), 'w') as f:
+    with open(os.path.join(output_path, "continuous_quantity_smoothness.json"), 'w') as f:
         json.dump(images_info, f, indent=4)
 
     print(f"{len(images_info['shape_section'])} images generated.")
