a23a3dc395d31edddc5ae93bf73b685931089d91
Xinyu Fang <CharlesFang207@outlook.com>
2024-09-15 22:08:16 -0400

    Shuffle the choices

--- extract__relative_index_info.py
+++ extract__relative_index_info.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 def read_json(file_path):
     with open(file_path, 'r') as file:
@@ -20,42 +21,57 @@ def process_data(data):
                 "scene": item["scene"]
             }
 
+            # Process object info
+            for obj in item["objects_info"]:
+                obj_type = obj["type"]
+                obj_color = obj["color"]
+                obj_size = obj["size"]
+
+                if obj_color not in sources[suffix]["objects"]:
+                    sources[suffix]["objects"][obj_color] = []
+                
+                sources[suffix]["objects"][obj_color].append({
+                    "type": obj_type,
+                    "size": obj_size
+                })
+
         # Append source image path
         sources[suffix]["source"].append("/data/shared/sim/benchmark/evaluation/datasets/relative_counting/tdw/images/" + image_path)
 
-        # Process object info
-        for obj in item["objects_info"]:
-            obj_type = obj["type"]
-            obj_color = obj["color"]
-            obj_size = obj["size"]
 
-            if obj_color not in sources[suffix]["objects"]:
-                sources[suffix]["objects"][obj_color] = []
-            
-            sources[suffix]["objects"][obj_color].append({
-                "type": obj_type,
-                "size": obj_size
-            })
 
     # Create the new data entries
     for key, value in sources.items():
+        color_1 = list(value["objects"].keys())[0]
+        color_2 = list(value["objects"].keys())[1] if len(value["objects"].keys()) > 1 else list(value["objects"].keys())[0]
+
+        choices = [color_1, color_2, "They are the same"]
+        random.shuffle(choices)
+
         obj_count = {color: len(objs) for color, objs in value["objects"].items()}
         num_objects = list(obj_count.values())
-        answer = num_objects.index(max(num_objects)) + 1 if len(set(num_objects)) > 1 else 3
+        
+        if len(set(num_objects)) > 1:
+            if num_objects.index(max(num_objects)) == 1:
+                answer = choices.index(color_2) + 1
+            else:
+                answer = choices.index(color_1) + 1
+        else:
+            answer = choices.index("They are the same") + 1
 
         background_map = {"tdw_room": 0, "monkey_physics_room": 1}
         background = background_map.get(value["scene"], 3)
 
         new_data.append({
             "source": value["source"],
-            "color1": list(value["objects"].keys())[0],
-            "color2": list(value["objects"].keys())[1] if len(value["objects"].keys()) > 1 else list(value["objects"].keys())[0],
+            "color1": color_1,
+            "color2": color_2,
             "num_objects1": num_objects[0],
             "num_objects2": num_objects[1] if len(num_objects) > 1 else num_objects[0],
             "background": background,
             "round": 0,  
             "objects": value["objects"],
-            "choices": [list(value["objects"].keys())[0], list(value["objects"].keys())[1] if len(value["objects"].keys()) > 1 else list(value["objects"].keys())[0], "They are the same"],
+            "choices": choices,
             "answer": answer
         })
 
